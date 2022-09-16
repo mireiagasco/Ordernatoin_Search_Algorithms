7e845b27b66fc7e6b9df68448fdd42c7f7458b7f
AinaPonseti <113668749+AinaPonseti@users.noreply.github.com>
2022-09-16 12:40:03 +0200

    Update SearchAlgorithms.java

--- src/SearchAlgorithms.java
+++ src/SearchAlgorithms.java
@@ -3,8 +3,60 @@ import java.util.List;
 import java.util.Random;
 
 public class SearchAlgorithms {
-
-    public static void main (String[] args){
+      public static void main(String [] args) {
+	    	//Cerca lineal a array desordenat
+	    	int array[] = new int[] { 12000, 3450, 1456, 45000, 356 };
+	    	int i=cercaLineal(array,1456);
+	    	comprovar(i);
+	    	//Cerca lineal a array ordenat
+	    	array = new int[] { 356, 1456, 3450, 12000, 45000};
+	    	i= cercaLineal(array,1456);
+	    	comprovar(i);
+	    	//Cerca binaria a array ordenat
+	    	i= cercaBinaria(array,12000);
+	    	comprovar(i);
+	    	
+	    }
+	    
+	    public static int cercaLineal(int arr[], int x)
+	    {
+	        for (int i = 0; i < arr.length; i++) {
+	            if (arr[i] == x)
+	                return i;
+	        }
+	        return -1;
+	    }
+	    
+	    public static int cercaBinaria(int array[], int x)
+	    {
+	    	int indMenor=0;
+	    	int indMajor=array.length-1;
+	        while (indMenor <= indMajor) {
+	            int indMitja = indMenor + (indMajor - indMenor) / 2;
+	     
+	            if (array[indMitja] == x)
+	                return indMitja;
+	     
+	            if (array[indMitja] < x)
+	                indMenor = indMitja + 1;
+	     
+	            else
+	                indMajor = indMitja - 1;
+	        }
+	     
+	        return -1;
+	    }
+	    
+	    public static void comprovar(int n) {
+	      	if(n !=-1) {
+	    		System.out.println("S'ha trobat; Posicio :" + n);
+	    	}
+	    	else
+	    		System.out.println("No s'ha trobat el nombre");
+	    	
+	    }
+	}
+    /*public static void main (String[] args){
         List<Integer> list = new ArrayList();
         Random rand = new Random();
 
@@ -67,5 +119,5 @@ public class SearchAlgorithms {
         int digit = number % (int) Math.pow(10, pos);
         digit = digit / (int) Math.pow(10, pos - 1);
         return (digit);
-    }
+    }*/
 }
